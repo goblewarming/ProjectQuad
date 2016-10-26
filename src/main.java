16c99f2b2d7b81fc2c3752c0086a78f378849e9c
timgo <timgo@10.206.10.127>
2016-10-26 02:53:17 -0400

    ya

---
+++ src/main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class main {
+
+	public static void main(String[] args){
+
+		ArrayList<String> str = new ArrayList<String>();
+		
+		str.add("Tim");
+		str.add(" is");
+		str.add(" cool!");
+		
+		for (int i=0; i<str.size(); i++){
+			System.out.print(str.get(i));
+		}
+		
+		
+		
+		
+	}}
+
